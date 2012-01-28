2263c1636b90974da19cbcc01a19163f04d87a98
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-01-28 14:58:36 +0000

    Added basic argument validation.

--- parser.pl
+++ parser.pl
@@ -4,3 +4,10 @@ use Modern::Perl;
 
 say "library-parser (c) 2012 Michal Huniewicz";
 say "www.m1key.me";
+
+if (@ARGV != 1) {
+    die "Error: You must specify exactly one parameter - path to library.";
+}
+
+my $libraryPath = $ARGV[0];
+printf "Using library file: [%s].\n", $libraryPath;
