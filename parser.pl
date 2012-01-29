20f313858ec6187caf3ace6973837d19fef34f51
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-01-29 01:00:42 +0000

    Added usage print.

--- parser.pl
+++ parser.pl
@@ -5,9 +5,14 @@ use Modern::Perl;
 say "library-parser (c) 2012 Michal Huniewicz";
 say "www.m1key.me";
 
+say "Usage: perl parser.pl <library file>";
+
+say "--------------------";
+
 if (@ARGV != 1) {
     die "Error: You must specify exactly one parameter - path to library.";
 }
 
 my $libraryPath = $ARGV[0];
 printf "Using library file: [%s].\n", $libraryPath;
+
