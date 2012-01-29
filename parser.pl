01f055450937a2d7cf049769ada17c5095181b65
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-01-29 02:11:33 +0000

    Print full song stats.

--- parser.pl
+++ parser.pl
@@ -12,8 +12,25 @@ printf "Using library file: [%s].\n", $libraryPath;
 open(my $in,  "<",  $libraryPath)  or die "Can't open library file.";
 say "Opened library file.";
 
+my $trackName = "";
+my $albumName = "";
+my $albumArtistName = "";
+my $artistName = "";
 while (<$in>) {
-    if (/<key>Play Count<\/key><integer>(\d+)/) {
-	print $1 . " ";
+    if (/<key>Name<\/key><string>(.+)<\/string>/) {
+	$trackName = $1;
+    } elsif (/<key>Album<\/key><string>(.+)<\/string>/) {
+	$albumName = $1;
+    } elsif (/<key>Album Artist<\/key><string>(.+)<\/string>/) {
+	$albumArtistName = $1;
+    } elsif (/<key>Artist<\/key><string>(.+)<\/string>/) {
+	$artistName = $1;
+    } elsif (/<key>Play Count<\/key><integer>(\d+)/) {
+	my $artist = $albumArtistName ? $albumArtistName : $artistName;
+	printf "%s - %s - %s: %d\n", $artist, $albumName, $trackName, $1;
+	$trackName = "";
+	$albumName = "";
+	$albumArtistName = "";
+	$artistName = "";	
     }
 }
