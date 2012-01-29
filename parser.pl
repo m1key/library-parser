fc4d3bf0ddd6829ff54951e4977dd4041a18bcc2
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-01-29 01:30:15 +0000

    Opening library file.

--- parser.pl
+++ parser.pl
@@ -9,3 +9,5 @@ require "arguments.pl";
 my $libraryPath = &getLibraryPath;
 printf "Using library file: [%s].\n", $libraryPath;
 
+open(my $in,  "<",  $libraryPath)  or die "Can't open library file.";
+say "Opened library file."
