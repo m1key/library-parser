a233d5b7d1cc03f4411c580559281b1b25578bc9
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2012-02-09 23:00:00 +0000

    Print output line in each condition now.

--- parser.pl
+++ parser.pl
@@ -29,25 +29,32 @@ my $playCount = "";
 while (<$in>) {
     if (/<key>Name<\/key><string>(.+)<\/string>/) {
 	$trackName = $1;
+	print $out $_;
     } elsif (/<key>Album<\/key><string>(.+)<\/string>/) {
 	$albumName = $1;
+	print $out $_;
     } elsif (/<key>Album Artist<\/key><string>(.+)<\/string>/) {
 	$albumArtistName = $1;
+	print $out $_;
     } elsif (/<key>Artist<\/key><string>(.+)<\/string>/) {
 	$artistName = $1;
+	print $out $_;
     } elsif (/<key>Total Time<\/key><integer>(.+)<\/integer>/) {
 	$totalTime = $1;
+	print $out $_;
     } elsif (/<key>Disc Number<\/key><integer>(.+)<\/integer>/) {
 	$discNumber = $1;
+	print $out $_;
     } elsif (/<key>Track Number<\/key><integer>(.+)<\/integer>/) {
 	$trackNumber = $1;
+	print $out $_;
     } elsif (/<key>Play Count<\/key><integer>(\d+)/) {
 	$playCount = $1;
 
 	my $artist = $albumArtistName ? $albumArtistName : $artistName;
 	my $playCountFromDb = $dbAccess->handleTrack($artist, $albumName, $trackName, $totalTime, $trackNumber, $discNumber);
 	my $totalPlayCount = $playCountFromDb + $playCount;
-	say "Total play count for $trackName: $totalPlayCount";
+	#say "Total play count for $trackName: $totalPlayCount";
 
 	$trackName = "";
 	$albumName = "";
@@ -57,9 +64,11 @@ while (<$in>) {
 	$discNumber = "";
 	$trackNumber = "";
 	$playCount = "";
+
+	print $out $_;
+    } else {
+	print $out $_;
     }
-    
-    print $out $_;
 }
 
 $dbAccess->disconnect();
